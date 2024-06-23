0f4a9b516fb5e4d559075cf6dd1ea43c5f03d3bb
dpdahal <dp48dahal@gmail.com>
2024-06-23 10:10:02 +0545

    if else

---
+++ controlstatement/main.py
@@ -0,0 +1,85 @@
+# x=6
+# y=7
+# print(x>y)
+
+
+# if x>y:
+#     print("x is greater then y")
+# elif x==y:
+#     print("x is equal to y")
+# else:
+#     print("y is greater then x")
+
+
+# a=5
+# b=7
+# c=60
+
+
+# if a>b and a>c:
+#     print("a is greater")
+# elif b>a and b>c:
+#     print("b is greater")
+# else:
+#     print("c is greater")
+
+# WAP to enter any number and check whether it is even or odd
+# num=int(input("Enter any number: "))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+# WAP to enter any number and check whether it is divisible by 3 and 5
+# a = int(input("Enter any number: "))
+# if a%3==0 and a%5==0:
+#     print("Divisible by 3 and 5")
+# else:
+#     print("Not divisible by 3 and 5")
+# WAP to enter age and check whether the person is eligible to vote or not >18 and 40<
+# WAP to enter five subject marks and calculate the total percentage and grade
+#  35-45 ->c
+#  45-60 ->b
+#  60-75 ->a
+#  75-100 ->a+
+#  0-35 ->fail
+
+
+# print("1. Add")
+# print("2. Sub")
+# print("3. Mul")
+# print("4. Div")
+# print("5. Mod")
+
+# choice = int(input("Enter your choice: "))
+
+# if choice==1:
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     print(a+b)
+# elif choice==2:
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     print(a-b)
+
+
+# nested if else
+
+# username =input("Enter username: ")
+# password = input("Enter password: ")
+
+# if username=='admin':
+#     if password=='admin':
+#         print("Welcome admin")
+#     else:
+#         print("Invalid password")
+
+# else:
+#     print("Invalid username")
+    
+
+# if username=="admin" and password=="admin":
+#     print("Welcome admin")
+# else:
+#     print("Invalid username or password")
+
+# match
---
+++ operators/main.py
@@ -0,0 +1,77 @@
+# what is operators?
+# Operators are special symbols in Python that carry out arithmetic or
+#  logical computation. The value that the operator operates on is called the operand.
+
+# Arithmetic operators: +, -, *, /, %, **, //
+# + addition
+# - subtraction
+# * multiplication
+# / division
+# // floor division
+# % modulo
+# ** exponentiation
+
+# print(10%3)
+
+# print(4**2)
+# print(19//5)
+# print(10/0)
+
+# name ='ram'
+# print('hello '+str(30))
+
+# WAP to enter any two numbers and perform all arithmetic operations on them.
+
+
+
+# Comparison operators: ==, !=, >, <, >=, <=
+# print(10==10)
+# print(10!=10)
+# print(10>30)
+# print(10<30)
+# print(10>=10)
+# print(10<=10)
+
+
+# Logical operators: and, or, not
+
+# print(10==10 and 10>30)
+# print(10==10 or 10>30)
+# print(not 10==10)
+
+
+# Assignment operators: =, +=, -=, *=, /=, %=, **=, //=
+# a=5
+# a+=50
+# print(a)
+
+
+# Identity operators: is, is not
+# Membership operators: in, not in
+
+# a=5
+# b=6
+# c=a
+# print(a is b)
+# print(a is c)
+# print(a is not b)
+
+# print('s' in 'sophia')
+# print('z'  in 'sophia')
+# print('z' not in 'sophia')
+
+# if, elif, else,nested if else
+
+# x=5
+# y=7
+# print(x>y)
+
+# bitwaise operators: &, |, ^, ~, <<, >>
+
+print(5&6)
+print(bin(5))
+print(bin(6))
+# 101
+# 110
+# 100
+print(bin(4))
